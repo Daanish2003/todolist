f1da6e7dd6c3fd2942c08b137e69f71361f8a016
Daanish Azim Sheikh <redchillies6786@gmail.com>
2024-04-03 20:12:41 +0530

    added edit, delete and completion handler

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,131 @@
+"use client"
 import Image from "next/image";
+import React, { useEffect } from "react";
+import { useState, use } from "react";
 
 export default function Home() {
+  const [title, setTitle] = useState<string>("");
+  const [desc, setDesc] = useState<string>("");
+  const [incompleteTask, setIncompleteTask] = useState<{title: string, desc: string} []>([]);
+  const [completedTask, setCompletedTask] = useState<{title: string, desc: string} []>([]);
+  const [editTask, setEditTask] = useState<{title: string, desc: string} []>([]);
+  // By clicking a add task button task should be added to the incompleted list
+  // by clicking the delete button task should be deleted from the list
+  // by clicking the checkbox the task should be moved to the completed list
+  // by clicking the checkbox again the task should be moved to the incompleted list
+ //  by  clicking the edit button the task should be edited
+
+  const onSubmit = (e: any) => {
+     e.preventDefault();
+     setIncompleteTask([...incompleteTask, {title, desc}]);
+     setTitle("");
+     setDesc("");
+  }
+
+  const incompleteDeleteHandler = (index: number) => {
+    let copyTask = [...incompleteTask];
+    copyTask.splice(index, 1);
+    setIncompleteTask(copyTask);
+  }
+
+  const DeleteHandler = (index: number) => {
+    let copyTask = [...completedTask];
+    copyTask.splice(index, 1);
+    setCompletedTask(copyTask);
+  }
+
+  const incompleteEditHandler = (index: number) => {
+    let copyTask = [...incompleteTask];
+    let task = copyTask[index];
+    setTitle(task.title);
+    setDesc(task.desc);
+    copyTask.splice(index, 1);
+    setIncompleteTask(copyTask);
+  }
+
+  const EditHandler = (index: number) => {
+    let copyTask = [...completedTask];
+    let task = copyTask[index];
+    setTitle(task.title);
+    setDesc(task.desc);
+    copyTask.splice(index, 1);
+    setCompletedTask(copyTask);
+  }
+
+  const checkHandler = (index: number) => {
+    let copyTask = [...incompleteTask];
+    let task = copyTask[index];
+    let copyCompletedTask = [...completedTask];
+    copyCompletedTask.push(task);
+    setCompletedTask(copyCompletedTask);
+    copyTask.splice(index, 1);
+    setIncompleteTask(copyTask);
+  }
+
+  const unCheckHandler = (index: number) => {
+    let copyTask = [...completedTask];
+    let task = copyTask[index];
+    let copyIncompleteTask = [...incompleteTask];
+    copyIncompleteTask.push(task);
+    setIncompleteTask(copyIncompleteTask);
+    copyTask.splice(index, 1);
+    setCompletedTask(copyTask);
+  }
+  
+  
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+   <>
+    <h1 className="bg-black text-white p-5 text-5xl font-bold text-center">My Todo list</h1>
+    <form onSubmit={onSubmit}>
+      <input type="text" className="text-2xl border-zinc-800 border-2 m-5 px-4 py-2"
+         placeholder="Enter task here"
+         value={title}
+         onChange= {(e) => {
+          setTitle(e.target.value)
+         }}
+      />
+      <input type="text" className="text-2xl border-zinc-800 border-2 m-5 px-4 py-2"
+         placeholder="Enter description here"
+         value={desc}
+         onChange= {(e) => {
+          setDesc(e.target.value)
+         }}
+      />
+      <button className="bg-black text-white px-4 py-2 text-2xl m-5 rounded-lg">Add Task</button>
+    </form>
+    <hr />
+    <div className="p-8 bg-slate-200 mb-5">
+        <h2 className="font-bold text-xl">Incomplete</h2>
+        {incompleteTask.map((task, index) => (
+          <div key={index} className="border-2 border-zinc-800 flex items-center justify-between">
+            <div className="flex items-center">
+            <h3>{task.title}</h3>
+            </div>
+            <p>{task.desc}</p>
+            <div className="space-x-1">
+            <button className="bg-black text-white px-4 py-2 text-sm rounded-lg" onClick={() => incompleteDeleteHandler(index)}>Delete</button>
+            <button className="bg-black text-white px-4 py-2 text-sm rounded-lg" onClick={() => incompleteEditHandler(index)}>Edit</button>
+            <button  className="bg-black text-white px-4 py-2 text-sm rounded-lg" onClick={() => checkHandler(index)}>Completed</button>
+            </div>
+          </div>
+        ))}
+    </div>
+    <div className="p-8 bg-slate-200 mb-5">
+      <h2 className="font-bold text-xl">Complete</h2>
+    {completedTask.map((task, index) => (
+          <div key={index} className="border-2 border-zinc-800 flex items-center justify-between">
+            <div className="flex items-center">
+            <h3>{task.title}</h3>
+            </div>
+            <p>{task.desc}</p>
+            <div className="space-x-1">
+            <button className="bg-black text-white px-4 py-2 text-sm rounded-lg" onClick={() => DeleteHandler(index)}>Delete</button>
+            <button className="bg-black text-white px-4 py-2 text-sm rounded-lg" onClick={() => EditHandler(index)}>Edit</button>
+            <button  className="bg-black text-white px-4 py-2 text-sm rounded-lg" onClick={() => unCheckHandler(index)}>inComplete</button>
+            </div>
+          </div>
+        ))}
+    </div>
+   </>
   );
 }
